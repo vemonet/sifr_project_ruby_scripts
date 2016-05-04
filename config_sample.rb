520eb09ce579b28ba7a4e6c8f0ada65165a30cb4
Vincent Emonet <vincent.emonet@gmail.com>
2016-05-04 10:34:37 -0700

    update config sample

--- config_sample.rb
+++ config_sample.rb
@@ -16,10 +16,40 @@ def stageportal_apikey
   ''
 end
 
-def new_sifr_apikey
+def vm_apikey
   ''
 end
 
-def vm_apikey
-  ''
+
+
+# Variables for Google Analytics
+
+def analytics_service_account_email_address(portal)
+  if portal.eql?("stageportal")
+    ""
+  elsif portal.eql?("agroportal")
+    ""
+  elsif portal.eql?("bioportal")
+    ""
+  end
+end
+
+def analytics_path_to_key_file(portal)
+  if portal.eql?("stageportal")
+    "#{__dir__}/stageportal-#########.p12"
+  elsif portal.eql?("agroportal")
+    "#{__dir__}/agroportal-########.p12"
+  elsif portal.eql?("bioportal")
+    "#{__dir__}/bioportal-#########.p12"
+  end
+end
+
+def analytics_profile_id(portal)
+  if portal.eql?("stageportal")
+    "ga:#########"
+  elsif portal.eql?("agroportal")
+    "ga:#######"
+  elsif portal.eql?("bioportal")
+    "ga:#######"
+  end
 end
